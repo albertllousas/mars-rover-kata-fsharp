625f8821286164fbda40540ace71159686185662
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-12-09 07:20:03 +0100

    Refactor: move commands as functions in their own modoule to improve readability and modularity

--- src/MarsRover.fs
+++ src/MarsRover.fs
@@ -6,13 +6,19 @@ type Rover = { Dir: Direction; Pos : int * int }
 
 type Command = MoveForward | MoveBackward
 
+module Commands =
+  
+  let moveForward ({ Dir = dir; Pos = (x, y)  } as rover) =
+     match dir with | N -> (x, y + 1) | W -> (x - 1, y) | S -> (x, y - 1) | E -> (x + 1, y)
+     |> fun pos -> { rover with Pos = pos}
+  
+  let moveBackward ({ Dir = dir; Pos = (x, y)  } as rover)=
+    match dir with | N -> (x, y - 1) | W -> (x + 1, y) | S -> (x, y + 1) | E -> (x - 1, y)
+        |> fun pos -> { rover with Pos = pos}
+
 module Rover = 
   
-  let executeCommand cmd ({ Dir = dir; Pos = (x, y)  } as rover) =
+  let executeCommand cmd rover =
     match cmd with
-      | MoveForward ->
-        match dir with | N -> (x, y + 1) | W -> (x - 1, y) | S -> (x, y - 1) | E -> (x + 1, y)
-        |> fun pos -> { rover with Pos = pos}
-      | MoveBackward ->
-        match dir with | N -> (x, y - 1) | W -> (x + 1, y) | S -> (x, y + 1) | E -> (x - 1, y)
-        |> fun pos -> { rover with Pos = pos}
+      | MoveForward -> Commands.moveForward rover
+      | MoveBackward -> Commands.moveBackward rover
